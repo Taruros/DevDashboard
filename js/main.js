e6058d16a9e9e98bc9bae8f20a198b723926d3f8
Taruros <szymonkwiatkowski@onet.pl>
2025-10-31 21:01:34 +0100

    Style improvements and color changes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import { initTheme } from "./theme.js";
 import { initDate } from "./date.js";
 import { initTodo } from "./todo.js";
+import { initWeather } from "./weather.js";
 
 initTheme();
 initDate();
 initTodo();
+initWeather();
